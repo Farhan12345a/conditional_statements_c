6ba8c58f25090285f7df1b38aedde2623eeaedab
Farhan Shahbaz <49499937+Farhan12345a@users.noreply.github.com>
2020-03-02 22:17:39 -0500

    Add files via upload

---
+++ partici_10.c
@@ -0,0 +1,63 @@
+//Farhan Shahbaz
+
+//Wayne
+
+
+
+#include <stdio.h>
+
+
+
+int main()
+{
+
+    char temp[10];
+
+    puts("Please enter your temperature:");
+
+    fgets(temp, sizeof(temp), stdin);
+
+    printf("Temperature: %s\n", temp);
+
+    
+ //String to number
+
+     
+      char* rem;
+
+      double dbl = strtod(temp, &rem);
+
+      //printf("dbl value : %s\n",temp);
+
+      printf("The number is %d\n", dbl);
+
+    
+   
+      int sickness = fever(dbl);
+
+      if(sickness == 1){
+
+        printf("Your temperature is %s and you have a fever", temp);
+
+      }else{
+
+        printf("Your temperature is %s and you don't have a fever", temp);
+    
+}
+    
+
+return 0;
+
+
+}
+
+
+
+int fever(double &t){
+
+    int a = (t > 98.6) ? 1 : 0;
+
+    return a;
+    
+
+}
